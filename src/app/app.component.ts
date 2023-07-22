6647e5668f78132549c6630e5cacbf70c51463ea
Pedro Vieira <pvieiragontijo@gmail.com>
2023-07-22 09:37:30 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'calculo-data';
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      birthDate: [null],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  birthDataValue() {
+    console.log(this.form.value.birthDate);
+  }
+
+  calculateDate() {
+    let birthDate = this.form.value.birthDate.split('-');
+    return this.valueReducer(birthDate[2] + birthDate[1] + birthDate[0]);
+  }
+
+  valueReducer(value: number) {
+    value = Math.abs(value);
+    const arrValue = Array.from(value.toString()).map(Number);
+    let reducedValue = arrValue.reduce((acc, current) => {
+      acc += current;
+      if (acc > 9 && acc != 11 && acc != 22) {
+        let arrAcc = Array.from(acc.toString()).map(Number);
+        acc = arrAcc[0] + arrAcc[1];
+      }
+      return acc;
+    });
+    console.log(reducedValue);
+    return reducedValue;
+  }
 }
